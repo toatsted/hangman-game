6738bbeb05c5976979e4900c217bdbcf7f6a1c5a
Sam <samueljacksonhicks@gmail.com>
2018-02-10 20:29:45 -0600

    can now add letters in correct position when pressed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,3 +6,45 @@
 			//put in graveyard
 		//else
 			//put into underscores in right position
+
+var word = "cars";
+
+var fword = ["_", "_", "_", "_"];
+
+var graveyard = [];
+
+document.addEventListener("keyup", function(event){
+	letter = event.key;
+
+	var inWord = false;
+	for(var i = 0; i < word.length; i++){
+		if(letter === word[i]){
+			fword[i] = letter;
+			inWord = true;
+		}
+	}
+
+	if(!inWord){
+		graveyard.forEach(function(index){
+			if(letter === index){}
+			else {
+				graveyard.push(letter);
+			}
+		});
+	}
+
+	hidden = "";
+	for(var i = 0; i < fword.length; i++) {
+		hidden += fword[i] + " ";
+	}
+
+	var grave = "";
+	for(var i = 0; i < graveyard.length; i++) {
+		grave += graveyard + " ";
+	}
+
+
+
+	document.querySelector("#game").innerHTML = hidden;
+	document.querySelector("#grave").innerHTML=  graveyard;
+})
