50707e18b07ff6f23e99b707fe8357e0efbbbc2e
Sam <samueljacksonhicks@gmail.com>
2018-02-10 18:38:23 -0600

    stuff

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+//picks random word and displays underscores acording to length of random word
+
+//when a person presses a key
+	//loop through word to try to find letter
+		//if not in word
+			//put in graveyard
+		//else
+			//put into underscores in right position
