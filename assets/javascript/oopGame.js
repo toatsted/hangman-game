3da05d04b49f136da790aff8715b32fbd8169532
Sam <samueljacksonhicks@gmail.com>
2018-02-17 00:11:44 -0600

    i dont know what i'm commiting here

---
+++ assets/javascript/oopGame.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+	let states = [
+			"Alabama", "Alaska", "Arizona", "Arkansas", "California",
+			"Colorado", "Connecticut", "Delaware", "Florida", "Georgia",
+			"Hawaii", "Idaho", "Illinois", "Indiana", "Iowa",
+			"Kansas", "Kentucky", "Lousisiana", "Maine", "Maryland",
+			"Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri",
+			"Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey",
+			"New Mexico", "New York", "North Carolina", "North Dakota", "Ohio",
+			"Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina",
+			"South Dakota", "Tennessee", "Texas", "Utah", "Vermont",
+			"Virgina", "Washington", "West Virgina", "Wisconsin", "Wyoming"
+		];
+
+	let wins = 0;
+
+	let game() = {
+		lives: 5,
+
+
+	};
+
+
+});
